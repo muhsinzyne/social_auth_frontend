15d089b76c5c44de4610462060edc5431507a455
salmanakzz <salmanakzz808@gmail.com>
2024-01-03 00:35:28 +0530

    Add (Init) Axios & REST APIs

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import ApiService from "./core/services/APIService";
 
 const app = createApp(App);
 
+ApiService.init(app);
 app.use(router);
 
 app.mount("#app");
